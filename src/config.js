be09b72b4a1695dcf9d4336f5eae7be98f03831c
Davi Jose Bernardes da Silva <davijose40@gmail.com>
2023-04-03 15:14:18 -0300

    fix ambient variables calling

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const development = import.meta.env.VITE_HOST_DEV
 
-const production = import.meta.env.VITE_HOST_PRO
+const production = import.meta.env.VITE_HOST_PROD
 
 const RESOURCEHOMEPOST = import.meta.env.VITE_RESOURCE_HOME_POST
 
